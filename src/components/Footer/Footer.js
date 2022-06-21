0d560c70d661fc295482c5934b688b732c2cc10b
Ramadhani Nur Sarjito <rama.dhaninursarjito@gmail.com>
2022-06-21 23:57:14 +0700

    Deploy

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Todo App by RamaDN &copy;</p>
+        </div>
+    )
+}
+
+export default Footer
